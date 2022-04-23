4b28e1f8a7d89a977edac9b80c9c6a7d8d8bfa9d
Sachin <heysachinkry@gmail.com>
2022-04-23 12:35:30 +0530

    hi

---
+++ JS-Basics/fun.js
@@ -0,0 +1,37 @@
+//..........Topic...............
+//Example 
+
+
+// Setup
+
+
+
+// Only change code below this line
+
+//*****************************************
+
+// //..........Access array data..............
+// //Example 
+// var ourArray = [50, 60, 70];
+// var ourData = ourArray[0]
+
+// // Setup
+// var myArray = [50, 60, 70];
+
+// // Only change code below this line
+// var myData = myArray[0];
+// alert(myData);
+
+//*****************************************
+
+// ..........Modify array data...............
+//Example 
+var ourArray = [50, 60, 70];
+ourArray[0] = 45;
+
+// Setup
+var myArray = [50, 60, 70];
+
+// Only change code below this line
+
+alert(ourArray.push(80));
